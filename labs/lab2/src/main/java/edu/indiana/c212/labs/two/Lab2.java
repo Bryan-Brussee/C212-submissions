e54c3402a2306f09237ff98aa3b655e77693343a
Bryan Davies Brussee <bbrussee@silo.soic.indiana.edu>
2014-09-05 12:04:00 -0400

    auto-commit

--- labs/lab2/src/main/java/edu/indiana/c212/labs/two/Lab2.java
+++ labs/lab2/src/main/java/edu/indiana/c212/labs/two/Lab2.java
@@ -1,26 +1,58 @@
 package edu.indiana.c212.labs.two;
 
 public class Lab2 {
-	public static int recursiveAdd(int x, int y){
-		//TODO 
-		//code to add numbers recursively
-		return -1;
+	public static int recursiveAdd(int x, int y) {
+		if  (y == 0) {
+			return x;
+		} else if (x == 0) {
+			return y;
+		} else {
+			return recursiveAdd(x + 1, y - 1);
+		}
 	}
 	public static int iterativeAdd(int x, int y){
-		//TODO  
-		//code to add numbers iteratively
-		return -1;
+		if  (y == 0) {
+			return x;
+		} else if (x == 0) {
+			return y;
+		} else {
+			while (y > 0) {
+				x++;
+				y--;
+		}
+			return x;
+		}
 	}
 	public static int iterativeMult(int x, int y){
-		//TODO 
-		//code to multiply numbers iteratively 
-		return -1;
+		if (x == 0) {
+			return 0;
+		} else if (y == 0) {
+			return 0;
+		} else {
+			int answer = 0;
+			while (y > 0) {
+				answer = iterativeAdd(answer, x);
+				y--;
+			}
+			return answer;	
+			}
 	}
+		
 	public static int iterativeExp(int x, int exp){	
-		//TODO
-		//code to find exponential of a number iteratively
-		return -1;
-	}
+		if (exp == 0) {
+			return 1;
+		} else if (exp == 1) {
+			return x;
+		} else {
+			int answer = 1;
+			while (exp > 0) {
+				answer = iterativeMult(x, answer);
+				exp--;
+				
+			}
+			return answer;
+		}
+		}
 
     public  static void printExpected(Object expected, Object actual){
         System.out.printf("Expected value is : %s, received value was: %s. \n", expected, actual);
