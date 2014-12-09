3c2198961fe25d2f20544929714f68bfbff5e957
Bryan Davies Brussee <bbrussee@silo.soic.indiana.edu>
2014-12-09 14:27:37 -0500

    auto-commit

--- project/src/main/java/edu/indiana/cs/c212/view/graphical/BoardPanel.java
+++ project/src/main/java/edu/indiana/cs/c212/view/graphical/BoardPanel.java
@@ -1,32 +1,38 @@
 package edu.indiana.cs.c212.view.graphical;
 
+import java.awt.MenuContainer;
+import java.awt.Point;
+import java.awt.Toolkit;
+import java.awt.event.AWTEventListener;
 import java.awt.event.ActionEvent;
-
-
-
-
 import java.awt.event.ActionListener;
+import java.awt.image.ImageObserver;
+import java.io.Serializable;
+import java.util.EventListener;
 import java.util.Observable;
 import java.util.Observer;
 
+import javax.accessibility.Accessible;
 import javax.swing.JPanel;
 
-public class BoardPanel extends JPanel implements ActionListener, Observer {
+public class BoardPanel extends JPanel implements ActionListener,
+                ImageObserver, MenuContainer, Serializable, EventListener, Observer,
+                Accessible {
 
-	
-	
-	
+        @Override
+        public void update(Observable o, Object arg) {
+                // TODO Auto-generated method stub
 
-	@Override
-	public void update(Observable o, Object arg) {
-		// TODO Auto-generated method stub
+        }
 
-	}
-
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
-
-	}
-
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            HexTile tile = (HexTile) e.getSource();
+            MoveEvent move = new MoveEvent(new Point(tile.getBoardX(),
+                    tile.getBoardY()), 0);
+            for (AWTEventListener l : Toolkit.getDefaultToolkit()
+                    .getAWTEventListeners()) {
+                l.eventDispatched(move);
+            }
+        }
 }
--- project/src/main/java/edu/indiana/cs/c212/view/graphical/HexTile.java
+++ project/src/main/java/edu/indiana/cs/c212/view/graphical/HexTile.java
@@ -1,7 +1,94 @@
 package edu.indiana.cs.c212.view.graphical;
 
+import java.awt.Color;
+import java.awt.Polygon;
+
 import javax.swing.JButton;
 
+import edu.indiana.cs.c212.board.Tile;
+import edu.indiana.cs.c212.gameMechanics.PlayerColor;
+
 public class HexTile extends JButton {
+	private int x;
+	private int y;
+	private int radius;
+	private Tile tile;
+	private Polygon hexTile;
+
+
+
+public HexTile(int x, int y, int radius, Tile tile) {
+	this.x = x;
+	this.y = y;
+	this.radius = radius;
+	this.tile = tile;
+	
+	int[] xarray = new int[6];
+	int[] yarray = new int[6];
+
+	for (int i = 0; i < xarray.length; i++) {
+		for (int a = 30; a < 360; a += 60) {
+			xarray[i] = (int) (this.radius * Math.cos(a));
+		}
+	}
+	
+	for (int i = 0; i < yarray.length; i++) {
+		for (int a = 30; a < 360; a += 60) {
+			yarray[i] = (int) (this.radius * Math.sin(a));
+		}
+	}
+
+	this.hexTile = new Polygon(xarray, yarray, 6);
+	
+}
+
+public final int getBoardX() {
+	return this.x;	
+}
+
+public final int getBoardY() {
+	return this.y;
+}
+
+
+public void paint(java.awt.Graphics g) {
+
+	PlayerColor hexcolor = tile.getColor();
+	
+	if (hexcolor == PlayerColor.RED) {
+		g.setColor(Color.RED);
+	} else if (hexcolor ==PlayerColor.BLUE) {
+		g.setColor(Color.BLUE);
+	} else {
+		g.setColor(Color.WHITE);
+	}
+	
+	g.drawPolygon(hexTile);
+}
+
+@Override
+public boolean contains(java.awt.Point p) {
+	
+	return hexTile.contains(p);
+	
+}
+
+@Override
+public boolean contains(int x, int y) {
+	
+	return hexTile.contains(x,y);
+	
+}
+
+protected void processMouseEvent(java.awt.event.MouseEvent e) {
+	
+}
+
+public void setRadius(int radius) {
+	this.radius = radius;
+	
+	
+}
+
 
 }
